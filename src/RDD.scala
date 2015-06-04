fb87594b6a1f7b6fd7933187618b661c7b2d6391
Bastiaan van Graafeiland <bastiaan89@live.nl>
2015-06-04 14:54:55 +0200

    add some stubs

--- src/RDD.scala
+++ src/RDD.scala
@@ -7,9 +7,14 @@ abstract class RDD[T] {
 
   def map[U](f: T => U): RDD[U] = new MappedRDD(this, f)
 
+  def flatMap[U](f: T => Seq[U]) : RDD[U] = ???
+
   def filter(f: T => Boolean): RDD[T] = new FilteredRDD(this, f)
 
   def union(other: RDD[T]): RDD[T] = new UnionRDD(Seq(this, other))
+
+  def intersection(other: RDD[T]) : RDD[T] = ???
+
 
 
   // Actions
@@ -19,4 +24,8 @@ abstract class RDD[T] {
   def collect(): Seq[T]
 
   def reduce(f: (T,T) => T): T = collect().reduce(f)
+
+  def first(): T = collect().head
+
+  def take(n: Int): Seq[T] = collect().take(n)
 }
