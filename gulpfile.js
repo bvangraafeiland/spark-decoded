55de57c6496c00e268770056ef23beb814f9065c
Bastiaan van Graafeiland <bastiaan89@live.nl>
2015-10-17 03:00:34 +0200

    tweak header gradient, update partition explanation, add pre-computed sequences part

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,5 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir.config.publicPath = '';
 elixir.config.assetsPath = '';
 elixir.config.css.less.pluginOptions = {paths:['node_modules']};
