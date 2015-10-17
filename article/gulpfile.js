162fe887aa9c25f9f5d170f585a76dfbd66522e7
Bastiaan van Graafeiland <bastiaan89@live.nl>
2015-10-17 14:03:55 +0200

    reorganize article directory

--- article/gulpfile.js
+++ article/gulpfile.js
@@ -1,10 +1,10 @@
 var elixir = require('laravel-elixir');
 
-elixir.config.publicPath = '';
+elixir.config.publicPath = 'public';
 elixir.config.assetsPath = '';
 elixir.config.css.less.pluginOptions = {paths:['node_modules']};
 
 elixir(function(mix) {
     mix.less('main.less');
-    mix.browserify('main.js', 'js/bundle.js');
+    mix.browserify('main.js');
 });
